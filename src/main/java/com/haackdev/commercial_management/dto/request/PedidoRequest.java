db3976b202d63a9b3dc947ae9a5f251094fc528e
Ruan Haack <ruan.haack03@gmail.com>
2026-06-10 22:38:59 -0300

    fix(pedido): adicionar validacao nos itens do PedidoRequest

--- src/main/java/com/haackdev/commercial_management/dto/request/PedidoRequest.java
+++ src/main/java/com/haackdev/commercial_management/dto/request/PedidoRequest.java
@@ -1,5 +1,6 @@
 package com.haackdev.commercial_management.dto.request;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ public record PedidoRequest(
     String condicaoPagamento,
     String parcelas,
     @NotEmpty(message = "O pedido deve conter pelo menos um item")
+    @Valid
     List<ItemPedidoRequest> itens
 ) {
 }
